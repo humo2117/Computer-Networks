7312d0d0ef8a9a81d669e914e651b6f8a3244874
humo2117 <62993874+humo2117@users.noreply.github.com>
2022-12-12 07:55:12 +0530

    Update StopAndWaitSimulation.cpp

--- StopAndWaitSimulation.cpp
+++ StopAndWaitSimulation.cpp
@@ -1,137 +1,240 @@
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <ctime>
+//networkProtocol
+#include <cstdio>
+#include <string>
+#define MAX_PKT 4
 
 using namespace std;
 
-#ifdef __linux__
-#define CLRSCR "clear"
-#else
-#define CLRSCR "cls"
-#endif
-
-class simulator
+typedef enum
 {
-    const int FRAME_SIZE = 1024;
-    char *channel;
-    int isACKRecieved;
-    int isFrameRecieved;
-    char *frame;
-    void sender();
-    void reciever();
-    void senderRecieveACK();
-    void resendFrame();
+  dat,
+  ack,
+  nak
+} frameKind;
 
-  public:
-    simulator();
-    ~simulator();
-    void simulate(int count, int isChannelNoisy);
+typedef enum
+{
+  wait,
+  frameArrival
+} eventType;
+
+typedef struct
+{
+  unsigned char data[MAX_PKT];
+} packet;
+
+typedef struct
+{
+  packet *info;
+  frameKind kind;
+  unsigned int seq;
+  unsigned int ack;
+} frame;
+
+class Protocol
+{
+public:
+  int sentSeq;
+  int receivedSeq;
+
+  packet dataPacket;
+  frame senderFrame, receiverFrame;
+
+  Protocol()
+  {
+    sentSeq = receivedSeq = -1;
+  }
+
+  int waitForEvent(eventType e)
+  {
+    return e == frameArrival;
+  }
+
+  string showkind(frameKind k) //display the event type
+  {
+    switch (k)
+    {
+    case dat:
+      return "data";
+      break;
+    case ack:
+      return "ack";
+      break;
+    case nak:
+      return "nak";
+      break;
+    }
+    return "";
+  }
+
+  // SENDER: Network -> Data Link Interface
+  void fromNetworkLayer(packet &i)
+  {
+    printf("\nEncapsulating Packet<data='%s'> ...", i.data);
+    senderFrame.seq = ++sentSeq;
+    senderFrame.kind = dat;
+    senderFrame.info = &i;
+  }
+
+  // SENDER: Data Link -> Physical Interface
+  void toPhysicalLayer(frame &f)
+  {
+    if (f.kind == dat)
+      printf("\nSending DataFrame<kind=%s, sequence=%i> to Physical Layer ...",
+             showkind(f.kind).c_str(), f.seq);
+    else
+      printf("\nSending ControlFrame<kind=%s, ack=%i> to Physical Layer ...",
+             showkind(f.kind).c_str(), f.ack);
+  }
+
+  // RECEIVER: Physical -> Data Link Interface
+  void fromPhysicalLayer(frame &f)
+  {
+    printf("\nReceived DataFrame<kind=%s, sequence=%i> from Physical Layer ...",
+           showkind(f.kind).c_str(), f.seq);
+    printf("\nValidating Sequence Number ... ");
+    if (receivedSeq != f.seq)
+      printf("\nDecapsulating Frame ...");
+    else
+    {
+      printf("\nDuplicate Frame Encountered ...");
+      printf("\nDiscarding Frame ...");
+    }
+  }
+
+  // RECEIVER: Data Link -> Network Interface
+  void toNetworkLayer(packet &p)
+  {
+    printf("\nSending Packet<data='%s'> to Network Layer ...", p.data);
+    receivedSeq = senderFrame.seq;
+    receiverFrame.seq = 0;
+    receiverFrame.kind = ack;
+    receiverFrame.ack = senderFrame.seq + 1;
+  }
 };
+class stopAndWait : public Protocol
+{
+public:
+  string buf;
+  eventType event;
+
+  bool flag, start;
+  int coeff, count, len, lim;
+
+  stopAndWait(string s, int t)
+  {
+    buf = s;
+    coeff = t;
+    lim = 1e6;
+    flag = false;
+    start = false;
+    count = 0;
+  }
+
+  void sender();
+  void receiver();
+};
+
+void stopAndWait::sender()
+{
+  if (!start)
+  {
+    lim = buf.length() % MAX_PKT == 0
+              ? buf.length() / MAX_PKT
+              : buf.length() / MAX_PKT + 1;
+    printf("\nDividing Data into Groups of %d-bytes Each ...", MAX_PKT);
+    start = !start;
+  }
+
+  printf("\n\nSENDER\n======");
+  if (count > 0)
+  {
+    if (count % coeff == 0)
+    {
+      printf("\nERROR: SIMULATED TIMEOUT ...");
+      flag = true;
+    }
+    else
+    {
+      printf("\nReceived ControlFrame<kind=%s, ack=%d> ...",
+             showkind(receiverFrame.kind).c_str(),
+             receiverFrame.ack);
+      if (flag)
+      {
+        count--;
+        flag = !flag;
+      }
+    }
+    if (receiverFrame.kind == nak || flag)
+    {
+      printf("\nResending Previous Frame ...");
+      count--;
+      flag = true;
+    }
+  }
+
+  if (count == lim)
+  {
+    printf("\n\nData '%s' Sent Successfully ...", buf.c_str());
+    exit(0);
+  }
+
+  while (count < lim)
+  {
+    while (event != wait)
+    {
+      if (!flag)
+      {
+        printf("\nEncapsulating Data D%d into a Packet ...", count + 1);
+        for (int i = 0; i < MAX_PKT; i++)
+          dataPacket.data[i] = buf[i + count * MAX_PKT];
+        printf("\nPassing Packet to Data Link Layer ...");
+        event = frameArrival;
+      }
+      if (waitForEvent(event))
+      {
+        if (!flag)
+          fromNetworkLayer(dataPacket);
+        toPhysicalLayer(senderFrame);
+        event = wait;
+      }
+      receiver();
+    }
+  }
+}
+
+void stopAndWait::receiver()
+{
+  printf("\n\nRECEIVER\n========");
+  if (event == wait)
+  {
+    fromPhysicalLayer(senderFrame);
+    if (!flag)
+    {
+      toNetworkLayer(dataPacket);
+      count++;
+    }
+    else
+      count += 2;
+    toPhysicalLayer(receiverFrame);
+    event = frameArrival;
+    sender();
+  }
+}
 
 int main()
 {
-    int choice, count;
-    simulator sim;
-    srand(time(0));
-    while (1)
-    {
-        system(CLRSCR);
-        cout << "### STOP AND WAIT PROTOCOL SIMULATOR ###" << endl;
-        cout << "1) Simulate stop and wait for noisy channel" << endl;
-        cout << "2) Simulate stop and wait for non-noisy channel" << endl;
-        cout << "Enter 0 to exit..." << endl;
-        cout << "Enter your choice: ";
-        cin >> choice;
-        switch (choice)
-        {
-        case 0:
-            return 0;
-        case 1:
-            cout << "Number of frames to simulate: ";
-            cin >> count;
-            sim.simulate(count, 1);
-            break;
-        case 2:
-            cout << "Number of frames to simulate: ";
-            cin >> count;
-            sim.simulate(count, 0);
-            break;
-        default:
-            cout << "ERROR: wrong choice please try again..." << endl;
-            cin.ignore();
-            cin.get();
-        }
-    }
-}
+  char temp[50];
+  printf("Enter Data: ");
+  scanf("%s", temp);
 
-simulator::simulator()
-{
-    this->channel = new char[FRAME_SIZE];
-    this->frame = new char[FRAME_SIZE];
-}
+  int temp2;
+  printf("Enter Simulation Noise (>=2): ");
+  scanf("%i", &temp2);
 
-simulator::~simulator()
-{
-    delete[] this->channel;
-    delete[] this->frame;
-}
+  stopAndWait *obj = new stopAndWait(string(temp), temp2);
+  obj->sender();
+  delete obj;
 
-void simulator::sender()
-{
-    cout << "Enter frame to send: ";
-    cin >> this->frame;
-    cout << "SENDER: Sending frame: " << this->frame << endl;
-    strcpy(this->channel, this->frame);
-}
-
-void simulator::reciever()
-{
-    while (!isFrameRecieved)
-        resendFrame();
-    cout << "Recieved Frame: " << channel << endl;
-    cout << "Sending ACK" << endl;
-}
-
-void simulator::resendFrame()
-{
-    cout << "SENDER: Timer timed out resending frame (either frame is lost or ACK is lost)" << endl;
-    this->isFrameRecieved = rand() % 2;
-    this->isACKRecieved = rand() % 2;
-}
-
-void simulator::senderRecieveACK()
-{
-    if (!isACKRecieved)
-    {
-        resendFrame();
-        reciever();
-    }
-
-    cout << "SENDER: Recieved ACK sending next frame if any" << endl;
-}
-
-void simulator::simulate(int count, int isChannelNoisy = 0)
-{
-    while (count > 0)
-    {
-        if (isChannelNoisy)
-        {
-            this->isACKRecieved = rand() % 2;
-
-            this->isFrameRecieved = rand() % 2;
-        }
-        else
-        {
-            this->isACKRecieved = 1;
-            this->isFrameRecieved = 1;
-        }
-        sender();
-        reciever();
-        senderRecieveACK();
-        count--;
-    }
-    cout << "press enter to continue..." << endl;
-    cin.ignore();
-    cin.get();
+  return 0;
 }
