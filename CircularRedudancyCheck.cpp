3b708564eef00d6579cfe97f9bc3c255164905a3
humo2117 <62993874+humo2117@users.noreply.github.com>
2022-12-12 07:52:34 +0530

    Update CircularRedudancyCheck.cpp

--- CircularRedudancyCheck.cpp
+++ CircularRedudancyCheck.cpp
@@ -1,125 +1,73 @@
 #include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <ctime>
-
+#include <string>
 using namespace std;
-
-char *crc(char *, char *); //function for crc check
-char *getReminder(char *, char *); //helper function to get reminder by dividing data with poulynomial
-char *substr(char *, int, int); //function to get substring of a data frame
-bool isFrameCorrect(char *, char *); //fucntion to check whether the recieved data is correct or not	
-void simulateNetwork(bool isNoisy = false); //function to simulate network
-
-int main(int argc, char *argv[])
+void ltrim(string &sub)
 {
-    while (true)
+    while (sub[0] != '1' && sub.length())
+        sub = sub.substr(1);
+}
+string xorr(string &s1, string &s2)
+{
+    if (s1.length() != s2.length())
+        throw "Equal Length Strings Needed for xor";
+    string res;
+    for (int i = 0; i < s1.length(); i++)
     {
-        int choice;
-        cout << "Circular Redundancy Check Simulator" << endl;
-        cout << "1) transmit over non-noisy channel" << endl;
-        cout << "2) transmit over noisy channel" << endl;
-        cout << "enter 0 to exit..." << endl;
-        cout << "Enter your choice: ";
-        cin >> choice;
-        switch (choice)
+        (s1[i] == s2[i]) ? res.append("0") : res.append("1");
+    }
+    return res;
+}
+string remainder(string dividend, string key)
+{
+    int pos = 0;
+    int keylen = key.length();
+    int divlen = dividend.length();
+    string chunk, temp;
+    while (pos < divlen)
+    {
+        if (chunk.length() < keylen)
         {
-        case 0:
-            return 1;
-        case 1:
-            simulateNetwork();
-            break;
-        case 2:
-            simulateNetwork(true);
-            break;
-        default:
-            cout << "Error: wrong choice try again...";
+            chunk += dividend[pos];
+            pos++;
         }
-        cin.ignore();
-        cin.get();
-    }
-}
-
-char *crc(char *dataFrame, char *genFunc)
-{
-    char *result = new char[strlen(dataFrame) + strlen(genFunc)];
-    strcpy(result, dataFrame);
-    strcat(result, getReminder(dataFrame, genFunc));
-    return result;
-}
-
-char *getReminder(char *dataFrame, char *genFunc)
-{
-    char *tempDataFrame = new char[strlen(dataFrame) + strlen(genFunc)];
-    strcpy(tempDataFrame, dataFrame);
-    for (int i = 0; i < strlen(genFunc) - 1; i++)
-    {
-        strcat(tempDataFrame, "0");
-    }
-    for (int i = 0, j = strlen(genFunc); j < strlen(tempDataFrame) + 1; j++, i++)
-    {
-        char *currentBlock = substr(tempDataFrame, i, j);
-        if (currentBlock[0] == '1')
+        if (chunk.length() == keylen)
         {
-            for (int k = 0; k < strlen(currentBlock); k++)
+            if (chunk[0] == '1')
             {
-                tempDataFrame[i + k] = ((currentBlock[k] - '0') ^ (genFunc[k] - '0')) + '0';
+                temp = xorr(chunk, key);
+                chunk = temp;
             }
+            ltrim(chunk);
         }
-        delete[] currentBlock;
     }
-    return substr(tempDataFrame, strlen(dataFrame), strlen(tempDataFrame));
+    while (chunk.length() < keylen-1){
+        chunk  = "0"+chunk;
+    }
+    return chunk;
 }
-
-char *substr(char *str, int i, int j)
+int main()
 {
-    char *retValue = new char[j - i + 1];
-    for (int k = 0; k < j - i; k++)
-    {
-        retValue[k] = str[i + k];
-    }
-    retValue[j - i] = '\0';
-    return retValue;
-}
-
-bool isFrameCorrect(char *dataFrame, char *genFunc)
-{
-    char *reminder = getReminder(dataFrame, genFunc);
-    if (atoi(reminder) == 0)
-    {
-        return true;
-    }
-    return false;
-}
-
-void simulateNetwork(bool isNoisy)
-{
-    srand(time(0));
-    int frameSize, genFuncSize;
-    char *dataFrame, *genFunc;
-    cout << "Enter frame size: ";
-    cin >> frameSize;
-    dataFrame = new char[frameSize];
-    cout << "Enter data frame: ";
-    cin >> dataFrame;
-    cout << "Enter generating function size: ";
-    cin >> genFuncSize;
-    genFunc = new char[genFuncSize];
-    cout << "Enter generating fucntion: ";
-    cin >> genFunc;
-    char *transmittedFrame = crc(dataFrame, genFunc);
-    cout << "Frame sent: " << transmittedFrame << endl;
-    if (isNoisy)
-    {
-        transmittedFrame[rand() % strlen(transmittedFrame)] = (rand() % 2) + '0';
-    }
-    cout << "Frame recieved: " << transmittedFrame << endl;
-    if (isFrameCorrect(transmittedFrame, genFunc))
-    {
-        cout << "Frame recieved correctly over the network" << endl;
-    }
+    string msg, key, rmsg;
+    cout << "Enter Binary Message > ";
+    cin >> msg;
+    cout << "Enter Key > ";
+    cin >> key;
+    string dividend = msg;
+    for (int i = 1; i < key.length(); i++)
+        dividend.append("0");
+    string crc= remainder(dividend, key);
+    string frame = msg + crc;
+    cout<<"CRC = "<<crc<<"\n";
+    cout << "The Frame Sent       = " << frame << "\n";
+    cout<<"NOISE SIMULATION\n";
+    cout << "Enter Recieved Frame > ";
+    cin >> rmsg;
+    string rem2 = remainder(rmsg, key);
+    cout<<"Recieved Message Remainder = "<<rem2<<"\n";
+    ltrim(rem2);
+    if (rem2 == "" && frame.length()==rmsg.length())
+        cout << "NO ERROR";
     else
-    {
-        cout << "Frame recieved incorrectly over the network" << endl;
-    }
+        cout << "ERROR FOUND";
+    return 0;
 }
